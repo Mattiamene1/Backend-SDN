593081e1bafc6f234cec298d18cdb46f3801cd90
GoldenRose01 <116004785+GoldenRose01@users.noreply.github.com>
2023-08-04 17:37:38 +0200

    fixed issues with code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios').default
 const cors = require('cors')
 const app = express()
 const port = 3000
-const BASE_URL = 'http://127.0.0.1:8082'
+const BASE_URL = 'http://127.0.0.1:3000'
 
 app.use(cors())
 app.get('/hosts', (req, res) => {
