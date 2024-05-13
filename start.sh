e93394f5a00bbd89dfbd140141a438d95d3644e9
Mattia Meneghin <52247535+Mattiamene1@users.noreply.github.com>
2024-05-13 20:36:33 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-gnome-terminal --title="backend" --working-directory=~/Backend-SDN/ -- npm start
+konsole --title="backend" --workdir=~/Backend-SDN/ -e npm start
+konsole --title="mininet" --workdir=~/Backend-SDN/ryu-app -e sudo mn --topo tree,3 --controller=remote
 
-gnome-terminal --title="mininet" --working-directory=~/Backend-SDN/ryu-app -- sudo mn --topo tree,3 --controller=remote
 
-gnome-terminal --title="remote" --working-directory=~/Backend-SDN/ryu-app --command "ryu-manager --observe-links ryu.app.simple_switch ryu.app.gui_topology.gui_topology"
+#gnome-terminal --title="backend" --working-directory=~/Backend-SDN/ -- npm start
+
+#gnome-terminal --title="mininet" --working-directory=~/Backend-SDN/ryu-app -- sudo mn --topo tree,3 --controller=remote
+
+#gnome-terminal --title="remote" --working-directory=~/Backend-SDN/ryu-app --command "ryu-manager --observe-links ryu.app.simple_switch ryu.app.gui_topology.gui_topology"
 
 
