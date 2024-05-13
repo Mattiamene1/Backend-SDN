d26525c6e3db47c1a45d80602cc1585094042b29
Mattia Meneghin <52247535+Mattiamene1@users.noreply.github.com>
2024-05-13 20:44:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-cd /Backend-SDN
-screen
-npm start
-echo -en "\033[1;1H"
+#!/bin/bash
 
-cd /Backend-SDN/ryu-app
-screen
-mn --topo tree,3 --controller=remote
-echo -en "\033[1;1H"
+# Open terminal window for Backend-SDN
+gnome-terminal --working-directory=/Backend-SDN -- npm start
 
-cd /Backend-SDN/ryu-app
-screen
-ryu-manager --observe-links ryu.app.simple_switch ryu.app.gui_topology.gui_topology
-echo -en "\033[1;1H"
+# Open terminal window for Ryu controller
+gnome-terminal --working-directory=/Backend-SDN/ryu-app -- mn --topo tree,3 --controller=remote
+
+# Open terminal window for Ryu manager
+gnome-terminal --working-directory=/Backend-SDN/ryu-app -- ryu-manager --observe-links ryu.app.simple_switch ryu.app.gui_topology.gui_topology
+
+
+#cd /Backend-SDN
+#screen
+#npm start
+#echo -en "\033[1;1H"
+
+#cd /Backend-SDN/ryu-app
+#screen
+#mn --topo tree,3 --controller=remote
+#echo -en "\033[1;1H"
+
+#cd /Backend-SDN/ryu-app
+#screen
+#ryu-manager --observe-links ryu.app.simple_switch ryu.app.gui_topology.gui_topology
+#echo -en "\033[1;1H"
 
 
 #gnome-terminal --title="backend" --working-directory=~/Backend-SDN/ -- npm start
